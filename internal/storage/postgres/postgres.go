0925d55848c4d092c55b1e157e212658b560aec8
Roman Shelekhov <shelekhoff@gmail.com>
2023-11-29 17:06:16 +0400

    add SaveURL method

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/rshelekhov/url-shortener/internal/storage"
 )
 
 type Storage struct {
@@ -27,4 +28,17 @@ func NewStorage(dsn string) (*Storage, error) {
 
 func (s *Storage) Close() error {
 	return s.db.Close()
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const fn = "storage.postgres.SaveURL"
+
+	var lastInsertId int64
+	err := s.db.QueryRow("INSERT INTO url(url, alias) VALUES($1, $2) RETURNING id", urlToSave, alias).Scan(&lastInsertId)
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", fn, storage.ErrURLExists)
+	}
+
+	return lastInsertId, nil
 }
